9765269ab8aabe22c74c39e136e93dba48ec4bda
Nishant Agrawal <nishant.agrawal@mtree.co.in>
2020-09-14 23:27:58 +0530

    Added Sign In Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {SignIn} from './'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello World
+        <SignIn />
       </header>
     </div>
   );
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import App from './App';
+import SignIn from './SignIn';
+
+export {
+    App,
+    SignIn
+};
